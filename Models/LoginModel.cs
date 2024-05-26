fb037879101438f38145aaa317d762183ab815db
hei karata <herishine00@gmail.com>
2024-05-26 03:22:53 +0300

    Updated UI and added LoginModel
    Updated Index.cshtml with new layout and added a login form. Paths to bootstrap CSS files in _Layout.cshtml were updated. Added LoginModel.cs with validation rules for username and password. Added new CSS file for styling. Imported necessary namespaces and tag helpers in _ViewImports.cshtml.

---
+++ Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DiabetesApp.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Please provide your username")]
+        [RegularExpression(@"^[A-Za-z]+\.[A-Za-z]+$", ErrorMessage = "Use the format Lastname.Firstname")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please provide your password")]
+        [PasswordPropertyText]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+        [MaxLength(12, ErrorMessage = "Password must be less than 12 characters long")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character")]
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
